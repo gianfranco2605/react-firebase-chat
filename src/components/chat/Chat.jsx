84a4e9f0c5e89fc823b95b618252721c8bba275e
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2024-04-18 16:55:12 +0200

    details component

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -27,7 +27,54 @@ const Chat = () => {
           <img src="./info.png" alt="" />
         </div>
       </div>
-      <div className="center"></div>
+      <div className="center">
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo,
+              sit ea assumenda odio laudantium asperiores praesentium amet et
+              velit corporis!
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo,
+              sit ea assumenda odio laudantium asperiores praesentium amet et
+              velit corporis!
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo,
+              sit ea assumenda odio laudantium asperiores praesentium amet et
+              velit corporis!
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <img
+              src="https://images.pexels.com/photos/10434596/pexels-photo-10434596.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo,
+              sit ea assumenda odio laudantium asperiores praesentium amet et
+              velit corporis!
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+      </div>
       <div className="bottom">
         <div className="icons">
           <img src="./img.png" alt="" />
